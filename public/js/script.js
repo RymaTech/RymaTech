a72ddc72afeb7c361666c79809be38f10542fe6d
Ryan McAvoy <ryanmcavoy@protonmail.com>
2019-04-24 23:09:36 +0100

    feature/ add mobile nav bar

---
+++ public/js/script.js
@@ -0,0 +1,24 @@
+const menu = document.getElementsByClassName('menu')[0];
+const menuCover = document.getElementsByClassName('menu-cover')[0];
+const mobileNav = document.getElementsByClassName('mobile-nav')[2];
+console.log(mobileNav);
+window.onscroll = () => {
+  const nav = document.getElementsByClassName('ul-container')[0];
+  if (this.scrollY <= 10) {
+    nav.style.backgroundColor = 'hsla(0, 0%, 0%, 0)';
+  } else {
+    nav.style.backgroundColor = 'hsla(0, 0%, 0%, 1)';
+    nav.style.transition = 'background-color 1.5s ease-out';
+  }
+};
+
+menu.addEventListener('click', () => {
+  menuCover.style.visibility = 'visible';
+  menuCover.style.transition = '5s ease-out';
+});
+
+mobileNav.addEventListener('click', () => {
+  if (menuCover.style.visibility === 'visible') {
+    menuCover.style.visibility = 'hidden';
+  }
+});
