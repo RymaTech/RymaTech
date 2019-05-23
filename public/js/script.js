a0bf17753397481ab2f08122cf3fc5a150c93c96
Ryan McAvoy <ryanmcavoy@protonmail.com>
2019-05-23 17:49:59 +0100

    add some more projects

--- public/js/script.js
+++ public/js/script.js
@@ -5,15 +5,6 @@ const projectsClick = document.getElementById("projectsClick");
 const techStackClick = document.getElementById("techStackClick");
 const ContactClick = document.getElementById("ContactClick");
 
-let count = true;
-let mobileArr = [
-  menu,
-  profileClick,
-  projectsClick,
-  techStackClick,
-  ContactClick
-];
-
 window.onscroll = () => {
   const nav = document.getElementsByClassName("ul-container")[0];
   if (this.scrollY <= 10) {
@@ -24,20 +15,32 @@ window.onscroll = () => {
   }
 };
 
-const controlMobileNavBar = () => {
-  if (count) {
-    document.getElementById("mySidenav").style.width = "250px";
-    menu.src = "./public/images/x.png";
-    count = false;
-  } else {
-    document.getElementById("mySidenav").style.width = "0";
-    menu.src = "./public/images/menu.png";
-    count = true;
-  }
-};
+const mobileNavFunctionality = () => {
+  let count = true;
+  let mobileArr = [
+    menu,
+    profileClick,
+    projectsClick,
+    techStackClick,
+    ContactClick
+  ];
 
-mobileArr.map(x => {
-  x.addEventListener("click", () => {
-    controlMobileNavBar();
+  const controlMobileNavBar = () => {
+    if (count) {
+      document.getElementById("mySidenav").style.width = "250px";
+      menu.src = "./public/images/x.png";
+      count = false;
+    } else {
+      document.getElementById("mySidenav").style.width = "0";
+      menu.src = "./public/images/menu.png";
+      count = true;
+    }
+  };
+
+  mobileArr.map(x => {
+    x.addEventListener("click", () => {
+      controlMobileNavBar();
+    });
   });
-});
+};
+mobileNavFunctionality();
